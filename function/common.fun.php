0e58172d5a6ef353b91b3eed5ad1fc3a94dae536
ct <1176079760@qq.com>
2018-06-11 22:22:37 +0800

    add first

---
+++ function/common.fun.php
@@ -0,0 +1,120 @@
+<?php 
+require_once "./extr/speech/AipSpeech.php";
+require_once './config/config.local.php';
+
+/**
+生成指定长度的随机字符串
+@param  $length  生成字符长度
+@param  $specialChars  是否使用特殊字符
+*/
+function randString($length=10, $specialChars = false) {
+    $chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    if ($specialChars) {
+        $chars .= '!@#$%^&*()';
+    }
+
+    $result = '';
+    $max = strlen($chars) - 1;
+    for ($i = 0; $i < $length; $i++) {
+        $result .= $chars[rand(0, $max)];
+    }
+    return $result;
+}
+
+function save_upload_file($file, $save_dir='./upload')
+{
+	 // 取得上传文件信息
+    $fileName = $file['name'];
+    $fileType = $file['type'];
+    $fileError = $file['error'];
+    $fileSize = $file['size'];
+    $tempName = $file['tmp_name'];
+    
+    // 定义上传文件类型
+    // $typeList = array('audio/wave', 'audio/wav', 'audio/x-wav', 'audio/x-pn-wav', 'audio/mpeg', 'audio/mp3');  //定义允许的类型
+    $typeList = array(
+    	'wav' => ['audio/wave', 'audio/wav', 'audio/x-wav', 'audio/x-pn-wav'],
+    	'mp3' => ['audio/mpeg', 'audio/mp3'],
+    	'aac' => ['audio/x-aac']
+	);
+
+    if ($fileError > 0) {
+            // 上传文件错误编号判断
+            switch ($fileError) {
+                case 1:
+                    $message = "上传的文件超过了php.ini 中 upload_max_filesize 选项限制的值。"; 
+                    break;
+                case 2:
+                    $message = "上传文件的大小超过了 HTML 表单中 MAX_FILE_SIZE 选项指定的值。"; 
+                    break;
+                case 3:
+                    $message = "文件只有部分被上传。"; 
+                    break;
+                case 4:
+                    $message = "没有文件被上传。";
+                    break;
+                case 6:
+                    $message = "找不到临时文件夹。"; 
+                    break;
+                case 7:
+                    $message = "文件写入失败"; 
+                    break;
+                case 8:
+                    $message = "由于PHP的扩展程序中断了文件上传";
+                    break;
+            }
+            exit("文件上传失败：".$message);
+    }
+    if (!is_uploaded_file($tempName)) {
+        exit("不是通过HTTP POST方式上传上来的");
+    }
+    $extr_name = null;
+    foreach ($typeList as $key => $value) {
+    	if( in_array( $fileType, $value ) ){
+    		$extr_name = $key;
+    	}
+    }
+   
+    if ($fileSize > 100000) {
+        exit("上传文件超出限制大小");                                 // 对特定表单的上传文件限制大小
+    } else {
+    	if( !file_exists($save_dir) ){
+    		mkdir($save_dir);
+    		chmod($save_dir,0777);
+    	}
+        $fileName = $save_dir.'/'.randString().'.'.$extr_name;
+        if (move_uploaded_file($tempName, $fileName)) {
+            return $fileName;
+        } else {
+            return false;
+        }
+    }
+
+}
+
+// 转换音频格式到允许的格式
+function convert_to_allow($path,$to)
+{
+	$from = end(split($path,'.'));
+	if(!in_array($from,['wav'])){
+		return false;
+	}
+
+	return true;
+}
+
+function yuyin2txt($path)
+{
+	// 创建客户端对象
+	$client = new AipSpeech(APP_ID, API_KEY, SECRET_KEY);
+
+	echo "<pre>";
+	// 识别本地文件
+	$res_json = $client->asr(file_get_contents('16k.pcm'), 'pcm', 16000, array(
+	    'dev_pid' => 1536,
+	)) ;
+
+	print_r($res_json);
+}
+
+ ?>
--- test.php
+++ test.php
@@ -1,15 +1,18 @@
 <?php
-require_once "./extr/speech/AipSpeech.php";
-require_once './config/config.local.php';
+require_once "./function/common.fun.php";
 
-// 创建客户端对象
-$client = new AipSpeech(APP_ID, API_KEY, SECRET_KEY);
 
-echo "<pre>";
-// 识别本地文件
-$res_json = $client->asr(file_get_contents('16k.pcm'), 'pcm', 16000, array(
-    'dev_pid' => 1536,
-)) ;
 
-print_r($res_json);
-  ?>
+
+if(isset($_FILES['audioData'])){
+	$up_file = $_FILES['audioData'];
+	if( $path = save_upload_file($up_file) ){
+		exit(('文件保存在：'.$path));
+	}else{
+		exit('保存失败！');
+	}
+}else{
+	echo '没有任何数据！';
+}
+print_r($_FILES);
+?>
